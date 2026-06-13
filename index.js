f0d89066af0456179c9a2fd5e66d5185a37bb9fe
nawshadpro01-gif <nawshadprofessional01@gmail.com>
2026-06-14 01:43:21 +0600

    connrected js with the index file

---
+++ index.js
@@ -0,0 +1,13 @@
+let currentTab = "all"
+const tabActive = ["bg-nevy", "border-navy"]
+const tabInactive = ["bg-transparent", "text-slate-700", "border-state-200"]
+function switchTab(tab) {
+    console.log(tab)
+    const tabs = ["all", "interview", "rejected"];
+
+    for (const i of tabs) {
+        const tabName = document.getElementById("tab-" + i );
+
+        
+    }
+}
